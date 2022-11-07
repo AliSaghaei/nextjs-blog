fa6becd0d4d82e33b984a3f8cee415515cde0c36
AliSaghaei <79021514+AliSaghaei@users.noreply.github.com>
2022-11-07 10:12:49 -0800

    done till the end of routing

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,20 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import Date from '../components/date';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 
-export async function getStaticProps(){
+export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
-    props:{
+    props: {
       allPostsData,
     },
   };
 }
 
-export default function Home({allPostsData}) {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -30,11 +32,11 @@ export default function Home({allPostsData}) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
